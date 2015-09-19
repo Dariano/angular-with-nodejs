4d596414424ffa70bfd73437f27a87f18dc7d1a2
dariano <darianosoares@hotmail.com>
2015-09-19 01:51:54 -0300

    Removido configurações do dialogs do arquivo main.js

--- public/js/main.js
+++ public/js/main.js
@@ -21,13 +21,3 @@ app.config(function ($routeProvider) {
 	})
 	.otherwise({redirectTo: '/contatos'});
 });
-
-app.config(['dialogsProvider','$translateProvider',function(dialogsProvider,$translateProvider){
-		dialogsProvider.setSize('sm');
-
-		$translateProvider.translations('pt-br',{
-			DIALOGS_OK: "Ok",
-			DIALOGS_YES: "Sim",
-			DIALOGS_NO: "Não"
-		});
-}])
