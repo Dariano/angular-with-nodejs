62c01873e29b02ae8d307029574d40ee5b467a2b
dariano <darianosoares@hotmail.com>
2015-09-24 00:31:54 -0300

    Implementado o teste para remover o ultimo contato inserido.

--- test/e2e/adicionandoNovoContatoSpec.js
+++ test/e2e/adicionandoNovoContatoSpec.js
@@ -17,12 +17,28 @@ describe('Usuario do sistema', function(){
 		var nome = element(by.model('ctrl.contato.nome'));
 		var email = element(by.model('ctrl.contato.email'));
 		
-		nome.sendKeys('darinao');
+		nome.sendKeys('dariano');
 		email.sendKeys('dariano@outlook.com');
 		
 		element(by.css('.btn-primary')).click();
 		
 		expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/contatos');
+		
+	});
+	
+	it('deve remover o contato requerido', function(){
+		browser.get('#/contatos');
+		
+		var linhas = element.all(
+			by.repeater('contato in ctrl.contatos')
+		);		
+		
+		var ultimaLinha = element(by.repeater('contato in ctrl.contatos').row(3))
+		ultimaLinha.element(by.css('.btn-warning')).click();
+		
+		element(by.css('button.btn-default.ng-binding')).click();
+		
+		expect(linhas.count()).toEqual(3);
 		
 	});
 	
